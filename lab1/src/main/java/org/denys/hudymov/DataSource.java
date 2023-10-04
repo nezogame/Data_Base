b9bcfb3d141d9922e061ce47f0cf28b0283b6801
Denys <denys.hudymov@gmail.com>
2023-10-04 13:21:48 +0300

    The class that implements Hikari connection pool

---
+++ lab1/src/main/java/org/denys/hudymov/DataSource.java
@@ -0,0 +1,33 @@
+package org.denys.hudymov;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+public class DataSource {
+
+    @Getter
+    private static HikariDataSource dataSource = null;
+
+    static {
+
+        HikariConfig config = new HikariConfig("/HotelDB.properties");
+        dataSource = new HikariDataSource(config);
+    }
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
