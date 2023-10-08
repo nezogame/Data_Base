41da4b2b18108695beccdc61bf55a92b4a946732
Denys <denys.hudymov@gmail.com>
2023-10-08 23:53:36 +0300

    Were added fields for entities and boilerplate code generated by Lombok

--- lab1/src/main/java/org/denys/hudymov/entity/HotelAccommodation.java
+++ lab1/src/main/java/org/denys/hudymov/entity/HotelAccommodation.java
@@ -1,4 +1,25 @@
 package org.denys.hudymov.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class HotelAccommodation {
+    private Long accommodationId;
+    private Long client_id;
+    private Long room_id;
+    private Timestamp arrivalDate;
+    private Timestamp departureDate;
+    private String note;
 }
