f67529a7d4585c6162c6d23b9097a0cca71891fe
Denys <denys.hudymov@gmail.com>
2023-10-08 23:46:59 +0300

    To client class has added all fields from the table, 2 setters and boilerplate code with Lombok

--- lab1/src/main/java/org/denys/hudymov/entity/Client.java
+++ lab1/src/main/java/org/denys/hudymov/entity/Client.java
@@ -1,4 +1,37 @@
 package org.denys.hudymov.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Client {
+    private Long clientId;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String passportData;
+    private String Comment;
+
+    public String getPatronymic() {
+        if(patronymic==null){
+            return "─";
+        }
+        return patronymic;
+    }
+
+    public String getPassportData() {
+        if(patronymic==null){
+            return "FRAUD";
+        }
+        return passportData;
+    }
 }
