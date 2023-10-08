9a12a27d344802432364df6a0b9aa0bb5e035583
Denys <denys.hudymov@gmail.com>
2023-10-08 23:55:14 +0300

    Now the main function only calls AppWindow constructor

--- lab1/src/main/java/org/denys/hudymov/App.java
+++ lab1/src/main/java/org/denys/hudymov/App.java
@@ -1,29 +1,13 @@
 package org.denys.hudymov;
 
-import org.denys.hudymov.repository.DataSource;
+import org.denys.hudymov.gui.AppWindow;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.*;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        try (Connection connection = DataSource.getConnection();
-             Statement statement = connection.createStatement()) {
+        JFrame appWindow = new AppWindow();
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM clients");
-            while (resultSet.next()) {
-                System.out.println("client id:" + resultSet.getInt("client_id"));
-                System.out.println("surname:" + resultSet.getString("surname"));
-                System.out.println("empName:" + resultSet.getString("name"));
-                System.out.println("patronymic:" + resultSet.getString("patronymic"));
-                System.out.println("passport data:" + resultSet.getString("passport_data"));
-                System.out.println("comment:" + resultSet.getString("comment"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
