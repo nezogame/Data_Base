544a2097c26c6325a358b6748ce3c6c00be0efd8
Denys <denys.hudymov@gmail.com>
2023-10-10 23:31:23 +0300

    I have created a class that implements crud interface for HotelAccommodation table

---
+++ lab1/src/main/java/org/denys/hudymov/repository/HotelAccommodationDao.java
@@ -0,0 +1,79 @@
+package org.denys.hudymov.repository;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.denys.hudymov.entity.HotelAccommodation;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@Builder
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class HotelAccommodationDao implements Dao<HotelAccommodation> {
+    @Override
+    public void create(HotelAccommodation entity) {
+
+    }
+
+    @Override
+    public void create(List<HotelAccommodation> entities) {
+
+    }
+
+    @Override
+    public List<HotelAccommodation> read() {
+        List<HotelAccommodation> hotelAccommodations = new ArrayList<>();
+        try (Connection connection = DataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM HotelAccommodations");
+            while (resultSet.next()) {
+                HotelAccommodation hotelAccommodation = HotelAccommodation
+                        .builder()
+                        .accommodationId(resultSet.getLong("accommodation_id"))
+                        .clientId(resultSet.getLong("client_id"))
+                        .roomId(resultSet.getLong("room_id"))
+                        .arrivalDate(resultSet.getTimestamp("arrival_date"))
+                        .departureDate(resultSet.getTimestamp("departure_date"))
+                        .note(resultSet.getString("note"))
+                        .build();
+                hotelAccommodations.add(hotelAccommodation);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return hotelAccommodations;
+    }
+
+    @Override
+    public Optional<List<HotelAccommodation>> get(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(HotelAccommodation entity) {
+
+    }
+
+    @Override
+    public void update(List<HotelAccommodation> entities) {
+
+    }
+
+    @Override
+    public void delete(int entityId) {
+
+    }
+}
