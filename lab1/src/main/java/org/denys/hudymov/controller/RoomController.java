ed61627554275d29fade5185ec6a11262bc00f31
Denys <denys.hudymov@gmail.com>
2023-10-12 00:04:07 +0300

    Were added methods for adding new room searching by room number because it unique

--- lab1/src/main/java/org/denys/hudymov/controller/RoomController.java
+++ lab1/src/main/java/org/denys/hudymov/controller/RoomController.java
@@ -5,10 +5,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.denys.hudymov.entity.Room;
 import org.denys.hudymov.repository.ClientDao;
 import org.denys.hudymov.repository.RoomDao;
 
 import javax.swing.text.html.Option;
+import java.sql.SQLException;
 import java.util.Optional;
 import java.util.Vector;
 
@@ -19,7 +21,8 @@ import java.util.Vector;
 @EqualsAndHashCode
 public class RoomController {
     private static final RoomDao ROOM_DAO = RoomDao.builder().build();
-    public  Vector<Vector<Object>> displayRooms(){
+
+    public Vector<Vector<Object>> displayRooms() {
         var clientsVector = new Vector<Vector<Object>>();
         var clients = ROOM_DAO.read();
         clients.forEach(room -> {
@@ -28,10 +31,26 @@ public class RoomController {
             row.add(room.getRoomNumber());
             row.add(room.getSeatsNumber());
             row.add(room.getComfort());
-            row.add(room.getPrice() != null ?  room.getPrice()+"$" : "NULL" );
+            row.add(room.getPrice() != null ? room.getPrice() + "$" : "NULL");
             row.add(room.getOccupied());
             clientsVector.add(row);
         });
         return clientsVector;
+    }
+
+    public void addRoom(String roomNumber, Integer seatsNumber, String comfort, String price) throws SQLException {
+        ROOM_DAO.create(
+                Room.builder()
+                .roomNumber(roomNumber)
+                .seatsNumber(seatsNumber)
+                .comfort(comfort)
+                .price(price)
+                .occupied(false)
+                .build()
+        );
+    }
+
+    public Room getRoomByNumber(String number) {
+        return ROOM_DAO.getByRoomNumber(number).get();
     }
 }
