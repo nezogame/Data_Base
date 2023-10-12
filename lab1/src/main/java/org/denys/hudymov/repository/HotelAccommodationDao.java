c17a6b22669983e2a1dffd39b879e07443899597
Denys <denys.hudymov@gmail.com>
2023-10-13 02:08:48 +0300

    Were created an insert, delete and update SQL query and implemented a query for updating and retrieving data by params and helper method for setting parameters to prepared statement

--- lab1/src/main/java/org/denys/hudymov/repository/HotelAccommodationDao.java
+++ lab1/src/main/java/org/denys/hudymov/repository/HotelAccommodationDao.java
@@ -5,12 +5,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.denys.hudymov.entity.HotelAccommodation;
+import org.denys.hudymov.entity.*;
+import org.jetbrains.annotations.*;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -21,9 +19,28 @@ import java.util.Optional;
 @ToString
 @EqualsAndHashCode
 public class HotelAccommodationDao implements Dao<HotelAccommodation> {
-    @Override
-    public void create(HotelAccommodation entity) {
 
+    private final String INSERT_SQL =
+            "INSERT INTO HotelAccommodations " +
+                    "VALUES(?,?,?,?,?)";
+
+    private final String UPDATE_SQL = "UPDATE HotelAccommodations SET client_id=?, room_id=?, arrival_date=?, " +
+            "departure_date=?, note=? " +
+            "WHERE accommodation_id=?";
+
+    private final String DELETE_SQL = "DELETE FROM  HotelAccommodations " +
+            "WHERE accommodation_id=?";
+    @Override
+    public void create(HotelAccommodation entity) throws SQLException{
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(INSERT_SQL)) {
+
+            setParameters(preparedStatement, entity);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException();
+        }
     }
 
     @Override
@@ -32,7 +49,7 @@ public class HotelAccommodationDao implements Dao<HotelAccommodation> {
         try (Connection connection = DataSource.getConnection();
              Statement statement = connection.createStatement()) {
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM HotelAccommodations");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM HotelAccommodations ORDER BY arrival_date DESC");
             while (resultSet.next()) {
                 HotelAccommodation hotelAccommodation = HotelAccommodation
                         .builder()
@@ -54,16 +71,81 @@ public class HotelAccommodationDao implements Dao<HotelAccommodation> {
 
     @Override
     public Optional<HotelAccommodation> get(long id) {
-        return Optional.empty();
+        Optional<HotelAccommodation> accommodation = Optional.ofNullable(null);
+        String SelectQuery = "SELECT * FROM HotelAccommodations WHERE accommodation_id=?";
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SelectQuery);
+        ) {
+
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) { // Check if there is a result
+                accommodation = Optional.of(HotelAccommodation
+                        .builder()
+                        .accommodationId(resultSet.getLong("accommodation_id"))
+                        .clientId(resultSet.getLong("client_id"))
+                        .roomId(resultSet.getLong("room_id"))
+                        .arrivalDate(resultSet.getTimestamp("arrival_date"))
+                        .departureDate(resultSet.getTimestamp("departure_date"))
+                        .note(resultSet.getString("note"))
+                        .build());
+            }
+
+            // Don't close the resultSet here.
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return accommodation;
     }
 
     @Override
     public void update(HotelAccommodation entity) {
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL)) {
 
+            setParameters(preparedStatement, entity);
+            preparedStatement.setLong(6, entity.getAccommodationId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(int entityId) {
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_SQL)) {
 
+            preparedStatement.setLong(1, entityId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setParameters(@NotNull PreparedStatement preparedStatement, @NotNull HotelAccommodation entity) throws SQLException {
+        preparedStatement.setLong(1, entity.getClientId());
+        preparedStatement.setLong(2, entity.getRoomId());
+        preparedStatement.setTimestamp(3, entity.getArrivalDate());
+        preparedStatement.setTimestamp(4, entity.getDepartureDate());
+        preparedStatement.setString(5, entity.getNote());
+    }
+
+    public List<Long> getAllId() {
+        List<Long> accommodationsId = new ArrayList<>();
+        String selectSQL = "SELECT accommodation_id FROM HotelAccommodations " +
+                "ORDER BY accommodation_id DESC ";
+        try (Connection connection = DataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(selectSQL);
+            while (resultSet.next()) {
+                accommodationsId.add(resultSet.getLong("accommodation_id"));
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return accommodationsId;
     }
 }
