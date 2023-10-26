1d569b531efe5f6e6047bc3610cb9882efe2975f
Denys <denys.hudymov@gmail.com>
2023-10-26 23:45:56 +0300

    Added additional validation method

--- lab1/src/main/java/org/denys/hudymov/model/Validator.java
+++ lab1/src/main/java/org/denys/hudymov/model/Validator.java
@@ -1,35 +1,38 @@
 package org.denys.hudymov.model;
 
-import lombok.Data;
-
-
-public class Validator  {
-    public static void validateTextField(String text, String source) throws IllegalArgumentException{
-        if(text.isEmpty()|| text.contains("Example:")){
-            throw new IllegalArgumentException("You need to write something in "+ source +" field!");
+public class Validator {
+    public static void validateTextField(String text, String source) throws IllegalArgumentException {
+        if (text.isEmpty() || text.contains("Example:")) {
+            throw new IllegalArgumentException("You need to write something in " + source + " field!");
         }
     }
 
-    public static void validateNumberOfSeats(String seatsNumber) throws IllegalArgumentException{
-        if(seatsNumber.isEmpty() ||  Integer.parseInt(seatsNumber)<=0){
+    public static void validateNumberOfSeats(String seatsNumber) throws IllegalArgumentException {
+        if (seatsNumber.isEmpty() || Integer.parseInt(seatsNumber) <= 0) {
             throw new IllegalArgumentException("The number of seats must be greater than 0!");
         }
     }
 
-    public static void validateComfort(String comfort) throws IllegalArgumentException{
-        if(comfort.contains("─")){
+    public static void validateStays(String seatsNumber) throws IllegalArgumentException {
+        if (seatsNumber.isEmpty() || Integer.parseInt(seatsNumber) < 0) {
+            throw new IllegalArgumentException("The number of seats must be greater or equals than 0!");
+        }
+    }
+
+    public static void validateComfort(String comfort) throws IllegalArgumentException {
+        if (comfort.contains("─")) {
             throw new IllegalArgumentException("You need to chose the room comfort");
         }
     }
 
-    public static void validatePrice(String price) throws IllegalArgumentException{
-        if(price.isEmpty() ||  Integer.parseInt(price)<=0) {
+    public static void validatePrice(String price) throws IllegalArgumentException {
+        if (price.isEmpty() || Integer.parseInt(price) <= 0) {
             throw new IllegalArgumentException("The price must be greater than 0!");
         }
     }
 
-    public static void validateDeleteSelection(String id) throws IllegalArgumentException{
-        if(id.contains("Select")) {
+    public static void validateDeleteSelection(String id) throws IllegalArgumentException {
+        if (id.contains("Select")) {
             throw new IllegalArgumentException("The price must be greater than 0!");
         }
     }
