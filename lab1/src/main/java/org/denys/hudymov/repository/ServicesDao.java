326bbefcd72cd4534aef29347a5c775b55be883f
Denys <denys.hudymov@gmail.com>
2023-12-08 00:54:47 +0200

    created DAO Services that implement crud interface

---
+++ lab1/src/main/java/org/denys/hudymov/repository/ServicesDao.java
@@ -0,0 +1,130 @@
+package org.denys.hudymov.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.ToString;
+import org.denys.hudymov.entity.Services;
+
+@Data
+@Builder
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class ServicesDao implements Dao<Services>{
+
+    private final String INSERT_SQL =
+            "INSERT INTO Services VALUES(?,?,?)";
+
+    private final String UPDATE_SQL=
+            "UPDATE Services SET price=?,category=?\n" +
+                    "WHERE service_name=?";
+
+    private final String DELETE_SQL = "DELETE FROM Services " +
+            "WHERE service_name=?";
+
+    @Override
+    public void create(Services entity) throws SQLException {
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(INSERT_SQL)) {
+
+            preparedStatement.setString(1, entity.getServiceName());
+            preparedStatement.setString(2, entity.getPrice());
+            preparedStatement.setString(3, entity.getCategory());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException();
+        }
+    }
+
+    @Override
+    public List<Services> read() {
+        List<Services> servicesCategories = new ArrayList<>();
+        try (Connection connection = DataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Services");
+            while (resultSet.next()) {
+                Services services = Services
+                        .builder()
+                        .serviceName(resultSet.getString("service_name"))
+                        .category(resultSet.getString("category"))
+                        .price(resultSet.getString("price"))
+                        .build();
+                servicesCategories.add(services);
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return servicesCategories;
+    }
+
+    @Override
+    public Optional<Services> get(long id) { return Optional.empty();}
+
+    public Optional<Services> get(String id) {
+        Optional<Services> services = Optional.empty();
+        String readSql = "SELECT * FROM Services " +
+                "WHERE category=?";
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(readSql)) {
+
+            preparedStatement.setString(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            services = Optional.ofNullable(Services
+                    .builder()
+                    .serviceName(resultSet.getString("service_name"))
+                    .price(resultSet.getString("price"))
+                    .category(resultSet.getString("categories"))
+                    .build());
+
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return services;
+    }
+
+    @SneakyThrows
+    @Override
+    public void update(Services entity) {
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_SQL)) {
+
+            preparedStatement.setString(1, entity.getServiceName());
+            preparedStatement.setString(2, entity.getPrice());
+            preparedStatement.setString(3, entity.getCategory());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(int entityId) throws SQLIntegrityConstraintViolationException {}
+
+    public void delete(String entityId) throws SQLIntegrityConstraintViolationException {
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_SQL)) {
+
+            preparedStatement.setString(1, entityId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLIntegrityConstraintViolationException(e.getMessage());
+        }
+    }
+}
