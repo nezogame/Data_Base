fc9edb5097871ceed987dd7ea44b1027dade617a
Denys <denys.hudymov@gmail.com>
2023-12-09 00:47:46 +0200

    Created methods in the service layer for the Staff class to interact with the data layer

--- lab1/src/main/java/org/denys/hudymov/service/StaffService.java
+++ lab1/src/main/java/org/denys/hudymov/service/StaffService.java
@@ -1,11 +1,17 @@
 package org.denys.hudymov.service;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
+import java.util.Optional;
 import java.util.Vector;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.denys.hudymov.entity.Staff;
 import org.denys.hudymov.repository.StaffDao;
 
 @Data
@@ -29,5 +35,40 @@ public class StaffService {
             clientsVector.add(row);
         });
         return clientsVector;
+    }
+
+    public void addStaff(String name, String email, Date employmentDate, String salary) throws SQLException {
+        STAFF_DAO.create(
+                Staff.builder()
+                        .name(name)
+                        .email(email)
+                        .employmentDate(employmentDate)
+                        .salary(salary)
+                        .build()
+        );
+    }
+
+    public List<Long> getIdList() {
+        return STAFF_DAO.getAllId();
+    }
+
+    public void updateStaff(Long id, String name, String email, Date date, String salary) {
+        STAFF_DAO.update(
+                Staff.builder()
+                        .staffId(id)
+                        .name(name)
+                        .email(email)
+                        .employmentDate(date)
+                        .salary(salary)
+                        .build()
+        );
+    }
+
+    public Staff getStaffById(Long id) {
+        return STAFF_DAO.get(id).get();
+    }
+
+    public void deleteStaff(String id) throws SQLIntegrityConstraintViolationException {
+        STAFF_DAO.delete(Integer.parseInt(id));
     }
 }
