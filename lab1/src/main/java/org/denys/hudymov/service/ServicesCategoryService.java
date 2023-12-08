b0cec7f5f19f0983738e63b82edfec814237fafb
Denys <denys.hudymov@gmail.com>
2023-12-09 00:48:23 +0200

    Created methods in the service layer for the ServicesCategory class to interact with the data layer

--- lab1/src/main/java/org/denys/hudymov/service/ServicesCategoryService.java
+++ lab1/src/main/java/org/denys/hudymov/service/ServicesCategoryService.java
@@ -1,11 +1,15 @@
 package org.denys.hudymov.service;
 
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
 import java.util.Vector;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.denys.hudymov.entity.ServicesCategory;
 import org.denys.hudymov.repository.ServicesCategoryDao;
 
 
@@ -27,5 +31,35 @@ public class ServicesCategoryService {
             clientsVector.add(row);
         });
         return clientsVector;
+    }
+
+    public List<String> getIdList() {
+        return SERVICES_CATEGORY_DAO.getAllId();
+    }
+
+    public void addCategory(String category, String description) throws SQLException {
+        SERVICES_CATEGORY_DAO.create(
+                ServicesCategory.builder()
+                        .category(category)
+                        .description(description)
+                        .build()
+        );
+    }
+
+    public void updateCategory(String category, String description) {
+        SERVICES_CATEGORY_DAO.update(
+                ServicesCategory.builder()
+                        .category(category)
+                        .description(description)
+                        .build()
+        );
+    }
+
+    public void deleteCategory(String category) throws SQLIntegrityConstraintViolationException {
+        SERVICES_CATEGORY_DAO.delete(category);
+    }
+
+    public ServicesCategory getCategoryById(String id) {
+        return SERVICES_CATEGORY_DAO.get(id).get();
     }
 }
