fc41529950061eb83399e5a4676faf8623767361
Denys <denys.hudymov@gmail.com>
2024-02-18 23:36:00 +0200

    I added 4 methods

--- lab4/src/main/java/org/denys/hudymov/lab4/repository/GroupRepository.java
+++ lab4/src/main/java/org/denys/hudymov/lab4/repository/GroupRepository.java
@@ -1,6 +1,15 @@
 package org.denys.hudymov.lab4.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.denys.hudymov.lab4.entity.Group;
 
 public interface GroupRepository extends Crud<Group, Long> {
+    Optional<Group> findGroupByName(String name);
+
+    Optional<Group> findGroupByNameAndWorkerQuantity(String name);
+
+    List<Group> findIncompleteGroups();
+
+    List<Group> findCompleteGroups();
 }
