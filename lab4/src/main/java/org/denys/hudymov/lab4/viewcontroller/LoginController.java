59531af5c70d6115c403c69bdcfe6d5e44180f33
Denys <denys.hudymov@gmail.com>
2024-02-18 23:48:55 +0200

    I created authentication method and validation for fields

--- lab4/src/main/java/org/denys/hudymov/lab4/viewcontroller/LoginController.java
+++ lab4/src/main/java/org/denys/hudymov/lab4/viewcontroller/LoginController.java
@@ -1,12 +1,27 @@
 package org.denys.hudymov.lab4.viewcontroller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import lombok.Data;
+import net.synedra.validatorfx.Validator;
+import org.denys.hudymov.lab4.repository.UserRepository;
+import org.denys.hudymov.lab4.repository.impl.UserRepositoryImpl;
 
+import static org.denys.hudymov.lab4.utilities.FxUtilities.displayInformationPopup;
+import static org.denys.hudymov.lab4.utilities.FxUtilities.switchScene;
+
+@Data
 public class LoginController {
+
+    private UserRepository userRepository = new UserRepositoryImpl();
+
+    private Validator validator = new Validator();
+
     @FXML
     private PasswordField passwordLoginText;
     @FXML
@@ -18,9 +33,38 @@ public class LoginController {
 
     private String password;
 
+
     @FXML
-    private void toggleVisiblePassword(){
-        if(passwordToggle.isSelected()) {
+    private void initialize() {
+        // Validate username
+        validator.createCheck()
+                .dependsOn("username", usernameLoginText.textProperty())
+                .withMethod(field -> {
+                    var userName = usernameLoginText.getText();
+                    if (userName.isEmpty()) {
+                        field.error("username can't be empty!");
+                    }
+                })
+                .decorates(usernameLoginText)
+                .immediate();
+
+        // Validate password
+        validator.createCheck()
+                .dependsOn("password", passwordLoginText.textProperty())
+                .withMethod(field -> {
+                    var userName = passwordLoginText.getText();
+                    if (userName.isEmpty()) {
+                        field.error("password can't be empty!");
+                    }
+                })
+                .decorates(passwordLoginText)
+                .immediate();
+    }
+
+
+    @FXML
+    private void toggleVisiblePassword() {
+        if (passwordToggle.isSelected()) {
             password = passwordLoginText.getText();
             passwordLoginText.clear();
             passwordLoginText.setPromptText(password);
@@ -29,7 +73,23 @@ public class LoginController {
 
         passwordLoginText.setText(password);
         passwordLoginText.setPromptText("Password");
+    }
 
+
+    /**
+     * Authentication by the name and password from log in page
+     */
+    @FXML
+    private void authenticate(ActionEvent event) {
+        var password = passwordLoginText.getText();
+        var username = usernameLoginText.getText();
+        if (password.isEmpty() && username.isEmpty()) {
+            displayInformationPopup("you violated text fields constraints!", Alert.AlertType.ERROR);
+            return;
+        }
+        if (userRepository.findUserRoleByNameAndPassword(username, password).isPresent()) {
+            switchScene(event, "main-view.fxml", "Main");
+        }
     }
 
 }
