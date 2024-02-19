0e897772f9c9f0519d921fbdb3ec11cd4a3a6a63
Denys <denys.hudymov@gmail.com>
2024-02-19 23:11:53 +0200

    I changed method signature

--- lab4/src/main/java/org/denys/hudymov/lab4/repository/UserRepository.java
+++ lab4/src/main/java/org/denys/hudymov/lab4/repository/UserRepository.java
@@ -6,12 +6,12 @@ import org.denys.hudymov.lab4.enums.Role;
 
 public interface UserRepository extends Crud<User, Long> {
     /**
-     * Return an Optional role of a searched user.
+     * Return an Optional of a searched user.
      * If name or password is null throw IllegalArgumentException.
      *
      * @param name     the name of the searched user. Must not be null.
      * @param password the password of the searched user. Must not be null.
      * @throws IllegalArgumentException in case the given name or password is null.
      */
-    Optional<Role> findUserRoleByNameAndPassword(String name, String password) throws IllegalArgumentException;
+    Optional<User> findUserByNameAndPassword(String name, String password) throws IllegalArgumentException;
 }
